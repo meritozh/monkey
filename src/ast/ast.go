937e2edb25957e050528f7fe4fa8802c9bfe3115
Gao Wanqiu <wanqiu.gao@ele.me>
2018-10-24 11:53:22 +0800

    [repl] integrate parsing phase

--- src/ast/ast.go
+++ src/ast/ast.go
@@ -254,9 +254,13 @@ func (bs *BlockStatement) TokenLiteral() string { return bs.Token.Literal }
 func (bs *BlockStatement) String() string {
 	var out bytes.Buffer
 
+	out.WriteString("{ ")
+
 	for _, s := range bs.Statements {
 		out.WriteString(s.String())
 	}
+
+	out.WriteString(" }")
 
 	return out.String()
 }
--- src/repl/repl.go
+++ src/repl/repl.go
@@ -10,7 +10,7 @@ import (
 	"fmt"
 	"io"
 	"lexer"
-	"token"
+	"parser"
 )
 
 // PROMPT prompt symbol
@@ -29,9 +29,21 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
+		program := p.ParseProgram()
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
